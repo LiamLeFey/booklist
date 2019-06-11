8d52413e6b727523a7f91859522590e4bfe2d3bb
William Gladen <liam@lefeys.com>
2019-06-11 13:08:21 -0600

    Defined a book, made sane defaults.

--- booklist.go
+++ booklist.go
@@ -1,3 +1,26 @@
 package main
 
-type Book struct {}
+import "time"
+
+type Status int
+
+const(
+   CheckedIn Status = iota
+   CheckedOut Status = iota
+)
+type Book struct {
+   Title, Author, Publisher string
+   PublishDate time.Time
+   Rating int
+   Status Status
+}
+
+func NewBook() Book {
+   return Book{
+      Author: "Unknown",
+      Title: "Untitled",
+      Publisher: "Not Published",
+      PublishDate: time.Now(),
+      Rating: 2,
+      Status: CheckedIn }
+}
--- booklist_test.go
+++ booklist_test.go
@@ -1,9 +1,46 @@
 package main
 
-import "testing"
+import (
+   "testing"
+   "time"
+)
 
 func TestBookStructExists(t *testing.T) {
    b := Book{}
    //oops need to use b
    b = b
+}
+func TestBookAttributeTypes(t *testing.T) {
+   b := Book{}
+   var tt string = b.Title
+   var a string = b.Author
+   var p string = b.Publisher
+   var d time.Time = b.PublishDate
+   var r int = b.Rating
+   var s Status = b.Status
+   tt, a, p, d, r, s = tt, a, p, d, r, s
+}
+func TestBookDefaultsSet(t *testing.T) {
+   before := time.Now()
+   b := NewBook()
+   after := time.Now()
+   if b.Title != "Untitled" {
+		t.Errorf("Unexpected Title")
+	}
+   if b.Author != "Unknown" {
+		t.Errorf("Unexpected Author")
+	}
+   if b.Publisher != "Not Published" {
+		t.Errorf("Unexpected Publisher")
+	}
+        // I find this funny.
+   if b.PublishDate.Before(before) || after.Before(b.PublishDate) {
+		t.Errorf("Unexpected Publish Date")
+	}
+   if b.Rating != 2 {
+		t.Errorf("Unexpected Rating")
+	}
+   if b.Status != CheckedIn {
+		t.Errorf("Unexpected Status")
+	}
 }
