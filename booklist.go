f420287efd89dd70a6b46b52851e8d46aa028c6e
William Gladen <liam@lefeys.com>
2019-06-11 13:11:12 -0600

    Ran gofmt.

--- booklist.go
+++ booklist.go
@@ -4,23 +4,24 @@ import "time"
 
 type Status int
 
-const(
-   CheckedIn Status = iota
-   CheckedOut Status = iota
+const (
+	CheckedIn  Status = iota
+	CheckedOut Status = iota
 )
+
 type Book struct {
-   Title, Author, Publisher string
-   PublishDate time.Time
-   Rating int
-   Status Status
+	Title, Author, Publisher string
+	PublishDate              time.Time
+	Rating                   int
+	Status                   Status
 }
 
 func NewBook() Book {
-   return Book{
-      Author: "Unknown",
-      Title: "Untitled",
-      Publisher: "Not Published",
-      PublishDate: time.Now(),
-      Rating: 2,
-      Status: CheckedIn }
+	return Book{
+		Author:      "Unknown",
+		Title:       "Untitled",
+		Publisher:   "Not Published",
+		PublishDate: time.Now(),
+		Rating:      2,
+		Status:      CheckedIn}
 }
